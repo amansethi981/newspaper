1e308f96f8ba961d190f9455d336770a44b9ae8b
amansethi981 <amansethi981@gmail.com>
2020-05-26 17:28:33 +0530

    Updates

---
+++ precache-manifest.167f68ac509beeb45d06c512c39f5305.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e354ebdb821f6ef2118f5184afdc6c14",
+    "url": "/newspaper/index.html"
+  },
+  {
+    "revision": "0417b5fe7f0359f48e12",
+    "url": "/newspaper/static/css/main.dce677b8.chunk.css"
+  },
+  {
+    "revision": "a6cd04593ac457dc1898",
+    "url": "/newspaper/static/js/2.de1f9222.chunk.js"
+  },
+  {
+    "revision": "0417b5fe7f0359f48e12",
+    "url": "/newspaper/static/js/main.5c8c6559.chunk.js"
+  },
+  {
+    "revision": "fa49a7975182e917a3ac",
+    "url": "/newspaper/static/js/runtime~main.f4c4b97b.js"
+  },
+  {
+    "revision": "67f1fede9d302c3faa14d94ea100826f",
+    "url": "/newspaper/static/media/stock coffee.67f1fede.jpg"
+  }
+]);
